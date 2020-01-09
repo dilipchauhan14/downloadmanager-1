9c4be054248f66faa33be7d19ea2956d7e1080fe
Dilip kumar chauhan <39065549+Dilipchauhan1998@users.noreply.github.com>
2020-01-09 11:24:23 +0530

    Add files via upload

---
+++ common_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+// This function is used to do setup before executing the test functions
+func TestMain(m *testing.M) {
+	//Set Gin to Test Mode
+	gin.SetMode(gin.TestMode)
+
+	// Run the other tests
+	os.Exit(m.Run())
+}
+
+// Helper function to create a router during testing
+func getRouter() *gin.Engine {
+	r := gin.Default()
+
+	//r.LoadHTMLGlob("templates/*")
+
+	return r
+}
+
+func processHttpRequest(r *gin.Engine, req *http.Request) *httptest.ResponseRecorder {
+	// Create a response recorder
+	w := httptest.NewRecorder()
+
+	// Create the service and process the above request.
+	r.ServeHTTP(w, req)
+
+	return w
+}
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
+
+func main() {
+	router = gin.Default()
+
+	InitializeRoutes()
+
+	router.Run(":8081")
+}
