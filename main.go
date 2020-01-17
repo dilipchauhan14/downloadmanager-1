86c5c9498bc3d56960277cfd1b4d166970b7c2e9
dilipchauhan14 <59637879+dilipchauhan14@users.noreply.github.com>
2020-01-17 11:26:53 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 )
-
+//A download manager is a software tool that manages the downloading of files from the Internet, which may be built: 
+//into a Web browser, or as a, usually more sophisticated, stand-alone manager.
 //gin router 
 var router *gin.Engine
 
